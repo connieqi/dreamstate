77f9ff771c05a189a06fba581c06f700bc695fde
Connie Qi <connieqi@umich.edu>
2018-06-17 23:16:53 -0700

    clean up

--- static/src/js/sketchbook.js
+++ static/src/js/sketchbook.js
@@ -1,18 +1,21 @@
-const NUM_SKETCHES = 22;
+const NUM_SKETCHES = 20;
+
+const captions = {
+    17: "A study of Magritte's \"The Listening Room\"",
+    19: "Drinks had in Paris, June 2018"
+}
+
+const captionStyle = {
+    backgroundColor: 'white'
+}
 
 const Images = React.createClass({
   render: function() {
-    const captionStyle = {
-        backgroundColor: 'white'
-    }
-
     const content = [];
     for (var i = NUM_SKETCHES - 1; i >= 0; i--) {
         content.push(<CenterImage src={`static/images/sketch${i}.jpg`} />)
-        if (i == 21) {
-            content.push(<div style={captionStyle}>Drinks had in Paris, June 2018</div>)
-        } else if (i == 19) {
-            content.push(<div style={captionStyle}>A study of Magritte's "The Listening Room"</div>)
+        if (i in captions){
+            content.push(<div style={captionStyle}>{captions[i]}</div>)
         }
     }
 
