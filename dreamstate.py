32692d8e1e5e1e8414efb058c5323323811c7034
Connie Qi <connieqi@umich.edu>
2018-08-12 18:44:45 -0700

    use gunicorn

--- dreamstate.py
+++ dreamstate.py
@@ -55,4 +55,4 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 80))
     print "*** running on port %s" % port
     
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
